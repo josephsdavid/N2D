f06d780d18515cd5dd65d510d57e5938e0c9bfc4
josephsdavid <josephsd@smu.edu>
2019-10-29 18:00:02 -0500

    frustrated

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
+setuptools.setup(
+    name = "n2d",
+    version = "0.0.1",
+    description = "(Not too) deep clustering",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/josephsdavid/N2D-OOP",
+    maintainer = "david Josephs",
+    maintainer_email = "josephsd@smu.edu",
+   #$ packages = setuptools.find_packages(exclude = [
+    #    "*weights*", "*viz*", "*data*"
+   # ]),
+    packages = ["src"],
+    license = 'MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'numpy',
+        'scikit-learn',
+        'umap-learn',
+        'tensorflow',
+        'scipy',
+        'h5py',
+        'matplotlib',
+        'seaborn',
+        'pandas',
+        'Keras'
+    ],
+    ext_modules = []
+)
